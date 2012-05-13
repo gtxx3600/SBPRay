4c8d59f33d81ef17660752ef01c182289522ff7b
sKabYY <skabyy@gmail.com>
2012-05-14 00:51:18 +0800

    fix bug

--- engine/RayTracingEngine.cc
+++ engine/RayTracingEngine.cc
@@ -18,7 +18,7 @@
 #include "base/Vec.h"
 const int RayTracingEngine::kMaxDepth = 6;
 #include <iostream>
-Vec light_dir = Vec(-1,-1,-1).Normalize();
+Vec light_dir = Vec(1,1,-1).Normalize();
 Color light = Color(0.5,0.5,0.5);
 Color RayTracingEngine::RayTracing(const Scene & scene, const Ray & ray, int depth, float diffuse_accumulation)
 {
@@ -36,9 +36,9 @@ Color RayTracingEngine::RayTracing(const Scene & scene, const Ray & ray, int dep
     if(m->diffusion > 0.1) {
       Color diffuse_color = Color::kBlack;
       float n_dot_l = inst.normal.DotProduct(light_dir);
-      if(n_dot_l > 0)
+      if(n_dot_l < 0)
       {
-        ret = ret.Add(light.Multiply(n_dot_l).Modulate(m->color));
+        ret = ret.Add(light.Multiply(-n_dot_l).Modulate(m->color));
         std::cout<<"hit\n";
       }
     }
@@ -47,7 +47,7 @@ Color RayTracingEngine::RayTracing(const Scene & scene, const Ray & ray, int dep
     if(m->reflection > 0.1) {
       Vec reflect_direction = ray.direction.Add(inst.normal.Multiply(
           2 * ray.direction.Negate().DotProduct(inst.normal))).Normalize();
-      Ray new_ray = Ray(inst.position, reflect_direction);
+      Ray new_ray = Ray(inst.position.Add(inst.normal.Multiply(0.001)), reflect_direction);
       Color new_color = RayTracing(scene, new_ray, depth + 1,
                   diffuse_accumulation + m->diffusion);
       ret = ret.Add(new_color.Multiply(m->reflection));
