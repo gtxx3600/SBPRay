eb9534e3e984815f7a1ed98b3baba7c25ac7ca47
Haofu Han <gtxx3600@gmail.com>
2012-05-14 00:36:29 +0800

    add raytracing for test

--- main.cc
+++ main.cc
@@ -14,19 +14,19 @@ using namespace std;
 #include "materials/Material.h"
 #include "pic/PPMFile.h"
 #include "engine/PathTracingEngine.h"
-
+#include "engine/RayTracingEngine.h"
 int main(int argc, char **argv) {
   cout << "start" << endl;
   float distance = 5.0;
   Camera camera(0.0,0.0,-distance, 0.0,1.0,0.0, 0.0,0.0,1.0, distance, 45);
   Scene scene;
-  Material source(0.0, 1.0, 0.0, Color::kWhite, Color::kRed);
-  Material material(0.0, 1.0, 0.0, Color::kBlack, Color::kGreen);
+  Material source(1.0, 0.0, 0.0, Color::kWhite, Color::kRed);
+  Material material(1.0, 0.0, 0.0, Color::kBlack, Color::kGreen);
   scene.CreateSphere(Vec(0, 0, 0), 0.5, source);
   scene.CreateSphere(Vec(1,1,0), 0.5, material);
 //  scene.CreateSphere(Vec(2, 2, 2), 2, material);
 //  scene.CreateSphere(Vec(-2, -2, 0), 3, material);
-  int size = 512;
+  int size = 256;
   int i = 0;
   Color *color_arr = new Color [size * size];
   for (int y = 0; y < size; y++) {
@@ -34,7 +34,8 @@ int main(int argc, char **argv) {
     for (int x = 0; x < size; x++) {
       float sx = static_cast<float>(x) / size;
       Ray ray = camera.GenerateRay(sx, sy);
-      Color color = PathTracingEngine::PathTracing(scene, ray, 0, 0);
+      //Color color = PathTracingEngine::PathTracing(scene, ray, 0, 0);
+      Color color = RayTracingEngine::RayTracing(scene, ray, 0, 0);
       color_arr[i++] = color;
     }
   }
