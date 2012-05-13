629efe3ab11d9eb9110a2d730fdca1ac1aa64957
Haofu Han <gtxx3600@gmail.com>
2012-05-13 09:49:51 +0800

    Rename world to scene, add camera

---
+++ scene/Camera.cc
@@ -0,0 +1,14 @@
+/*
+ * Camera.cc
+ *
+ *  Created on: 2012-5-13
+ *      Author: gtxx3600
+ */
+
+#include "Camera.h"
+
+Camera::Camera() {
+  // TODO Auto-generated constructor stub
+
+}
+
---
+++ scene/Camera.h
@@ -0,0 +1,16 @@
+/*
+ * Camera.h
+ *
+ *  Created on: 2012-5-13
+ *      Author: gtxx3600
+ */
+
+#ifndef CAMERA_H_
+#define CAMERA_H_
+
+class Camera {
+public:
+  Camera();
+};
+
+#endif /* CAMERA_H_ */
