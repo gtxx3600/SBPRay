2a14b66c238c58cb825f0b668c9963e6bf753a2d
Haofu Han <gtxx3600@gmail.com>
2012-05-13 19:11:19 +0800

    finish material

--- materials/Material.cc
+++ materials/Material.cc
@@ -6,8 +6,22 @@
  */
 
 #include "materials/Material.h"
+#include "base/Color.h"
 
 Material::Material() {
   // TODO Auto-generated constructor stub
+  diffusion = 0;
+  reflection = 0;
+  refraction = 0;
+  emittance = Color::kBlack;
+}
+
+Material::Material(float _diffusion, float _reflection, float _refraction, Color _emittance)
+:diffusion(_diffusion), reflection(_reflection), refraction(_refraction), emittance(_emittance){
 
 }
+
+Material::~Material(){
+}
+
+
