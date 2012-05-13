7b7c8a0bbaa9358b143e8859dcc537b295f45850
Haofu Han <gtxx3600@gmail.com>
2012-05-13 10:25:49 +0800

    finish class Vec

--- base/Vec.cc
+++ base/Vec.cc
@@ -4,11 +4,95 @@
  *  Created on: 2012-5-13
  *      Author: gtxx3600
  */
-
+#include <math.h>
 #include "Vec.h"
+
+const Vec Vec::kZero = Vec(0.0, 0.0, 0.0);
 
 Vec::Vec() {
   // TODO Auto-generated constructor stub
-
+  x = y = z = 0.0;
 }
+
+Vec::Vec(float a, float b, float c)
+:x(a),y(b),z(c)
+{
+}
+
+Vec Vec::Copy()
+{
+  return Vec(x,y,z);
+}
+
+
+
+Vec Vec::Normalize()
+{
+  float len = this->Length();
+  if (len > 0) {
+    return Vec(x/len, y/len, z/len);
+  } else {
+    return Vec(0,0,0);
+  }
+}
+
+
+
+Vec Vec::Negate()
+{
+  return Vec(-x, -y, -z);
+}
+
+
+
+Vec Vec::Add(const Vec & v)
+{
+  return Vec(x + v.x, y + v.y, z + v.z);
+}
+
+
+
+Vec Vec::Sub(const Vec & v)
+{
+  return Vec(x - v.x, y - v.y, z - v.z);
+}
+
+
+
+Vec Vec::Multiply(float val)
+{
+  return Vec(x*val, y*val, z*val);
+}
+
+
+
+Vec Vec::XProduct(const Vec & v)
+{
+  return Vec(y*v.z - v.y*z, z*v.x - v.z*x, x*v.y - v.x*y);
+}
+
+
+
+float Vec::Length()
+{
+  return sqrt(x*x + y*y + z*z);
+}
+
+
+
+float Vec::SqrLength()
+{
+  return x*x + y*y + z*z;
+}
+
+
+
+float Vec::DotProduct(const Vec & v)
+{
+  return x*v.x + y*v.y + z*v.z;
+}
+
+
+
+
 
--- base/Vec.h
+++ base/Vec.h
@@ -10,7 +10,20 @@
 
 class Vec {
 public:
+  float x,y,z;
+  static const Vec kZero;
   Vec();
+  Vec(float a, float b, float c);
+  Vec Copy();
+  Vec Normalize();
+  Vec Negate();
+  Vec Add(const Vec &v);
+  Vec Sub(const Vec &v);
+  Vec Multiply(float val);
+  Vec XProduct(const Vec &v);
+  float Length();
+  float SqrLength();
+  float DotProduct(const Vec &v);
 };
 
 #endif /* VEC_H_ */
