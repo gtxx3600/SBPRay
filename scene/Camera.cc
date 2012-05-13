ebe574eb4763492ea6294d8ecbd3c8ab479bac05
Haofu Han <gtxx3600@gmail.com>
2012-05-13 14:03:42 +0800

    finish camera

--- scene/Camera.cc
+++ scene/Camera.cc
@@ -6,21 +6,38 @@
  */
 
 #include "scene/Camera.h"
+#define _USE_MATH_DEFINES
+#include <cmath>
 #include "base/Vec.h"
+#include "base/Ray.h"
 
-
-Camera::Camera(const Vec &p, const Vec &u, const Vec &f, float d)
-    : position(p), upward(u), forward(f), distance(d) {
-  position.Normalize();
-  upward.Normalize();
-  forward.Normalize();
+Camera::Camera(const Vec &p, const Vec &u, const Vec &f, float d, float v)
+    : position(p), upward(u), forward(f), distance(d){
+  rightward = forward.XProduct(upward).Normalize();
+  upward = upward.Normalize();
+  forward = forward.Normalize().Multiply(distance);
+  fov_scale = tan(v * 0.5 * M_PI / 180) * 2;
 }
 
 Camera::Camera(float px, float py, float pz,
     float ux, float uy, float uz,
-    float fx, float fy, float fz, float d)
-    : position(px, py, pz), upward(ux, uy, uz), forward(fx, fy, fz), distance(d) {
-  position.Normalize();
-  upward.Normalize();
-  forward.Normalize();
+    float fx, float fy, float fz, float d, float v)
+    : position(px, py, pz), upward(ux, uy, uz), forward(fx, fy, fz),
+      distance(d){
+  rightward = forward.XProduct(upward).Normalize();
+  upward = upward.Normalize();
+  forward = forward.Normalize().Multiply(distance);
+  fov_scale = tan(v * 0.5 * M_PI / 180) * 2;
 }
+
+Ray Camera::GenerateRay(float x, float y)
+{
+  Vec r = rightward.Multiply((x - 0.5) * fov_scale);
+  Vec u = upward.Multiply((y - 0.5) * fov_scale);
+  return Ray(position, forward.Add(r).Add(u).Normalize());
+}
+
+
+
+
+
--- scene/Camera.h
+++ scene/Camera.h
@@ -9,18 +9,21 @@
 #define CAMERA_H_
 
 class Vec;
-
+class Ray;
 class Camera {
 public:
-  Camera(const Vec &p, const Vec &u, const Vec &f, float d);
+  Camera(const Vec &p, const Vec &u, const Vec &f, float d, float v);
   Camera(float px, float py, float pz,
       float ux, float uy, float uz,
-      float fx, float fy, float fz, float d);
+      float fx, float fy, float fz, float d, float v);
+  Ray GenerateRay(float x, float y);
 
   Vec position;
   Vec upward;
   Vec forward;
+  Vec rightward;
   float distance;
+  float fov_scale;
 };
 
 #endif /* CAMERA_H_ */
